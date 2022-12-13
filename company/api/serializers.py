9b9e43f1b4e3b310adfa84ba81d5f283a3386b5e
bipul077 <bipulratna07@gmail.com>
2022-12-13 17:04:37 +0545

    contactus api created

--- company/api/serializers.py
+++ company/api/serializers.py
@@ -5,3 +5,4 @@ from company.models import Contact
 class ContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contact
+        fields = ['id','uname','email','message']
--- company/api/urls.py
+++ company/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from company.api import views
 from rest_framework.routers import DefaultRouter
 
-router == DefaultRouter()
+router = DefaultRouter()
 router.register('contactapi',views.ContactModelViewSet,basename='contact')
 
 urlpatterns = [
