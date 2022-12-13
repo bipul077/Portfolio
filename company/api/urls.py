486a449af4368244539ff0d37b625da3403447fd
bipul077 <bipulratna07@gmail.com>
2022-12-13 16:05:11 +0545

    react updated

---
+++ company/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path,include
+from company.api import views
+from rest_framework.routers import DefaultRouter
+
+router == DefaultRouter()
+router.register('contactapi',views.ContactModelViewSet,basename='contact')
+
+urlpatterns = [
+    path('',include(router.urls)),
+]
