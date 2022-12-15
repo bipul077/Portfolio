d84080de4f3b6a36fa673a7d107d58301801dd30
sthaakin04 <akinstha97@gmail.com>
2022-12-15 11:00:10 +0545

    changes

--- portfolio/src/components/Home/Home.js
+++ portfolio/src/components/Home/Home.js
@@ -4,7 +4,7 @@ import './Home.css'
 export default function Home() {
   useEffect(() => {
     // let stars = document.getElementById('stars');
-    let moon = document.getElementById('moon');
+    // let moon = document.getElementById('moon');
     let behind = document.getElementById('mountains_behind');
     let text = document.getElementById('text');
     // let btn = document.getElementById('homebtn');
@@ -13,7 +13,7 @@ export default function Home() {
     window.addEventListener('scroll',function(){
       let value = window.scrollY;
       // stars.style.left = value * 0.25 + 'px';
-      moon.style.top = value * 1.05 + 'px';
+      // moon.style.top = value * 1.05 + 'px';
       behind.style.bottom = value * 0 + 'px';
       mountains_front.style.bottom = value * 0 + 'px';
       text.style.marginRight = value * 4 + 'px';
@@ -29,11 +29,11 @@ export default function Home() {
       </script>
       <section className='asection'>
         {/* <img src="images/stars.png" id="stars"/> */}
-        <img src="static/images/sun.png" id="moon"/>
-        <img src="static/images/swb1.png" id="mountains_behind"/>
+        {/* <img src="static/images/sun.png" id="moon"/> */}
+        <img src="static/images/hotelback.png" id="mountains_behind"/>
         <h2 className="text" id="text">Global Group Of Company</h2>
         {/* <a href="#" id="homebtn">Explore</a> */}
-        <img src="static/images/front.png" id="mountains_front"/>
+        <img src="static/images/hotelfront.png" id="mountains_front"/>
       </section>
       <HomeContents/>
     </div>
