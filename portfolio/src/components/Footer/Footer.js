2c515b2141dce6d3c4a161074a0090073ab76873
sthaakin04 <akinstha97@gmail.com>
2022-12-16 16:51:38 +0545

    changes

--- portfolio/src/components/Footer/Footer.js
+++ portfolio/src/components/Footer/Footer.js
@@ -17,7 +17,7 @@ export default function Footer() {
           <li><a href="#"><i className='fa fa-youtube'></i></a></li>
         </ul>
         <div className="text">
-          <h2>Global Group of Company</h2>
+          <h6>Global Group of Company</h6>
           <p>@Copyright issue 2022</p>
         </div>
       </div>
