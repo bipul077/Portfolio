f4f430e0a1e024af65b2761e2ff74185f3f0895b
sthaakin04 <akinstha97@gmail.com>
2022-12-18 14:11:38 +0545

    nothing

--- portfolio/src/components/Services/Services.js
+++ portfolio/src/components/Services/Services.js
@@ -32,15 +32,7 @@ export default function Services() {
             </div>
           </div>
 
-        <div className="row">
-          <div className="image">
-            <img src="static/images/s1.jpg"/>
-            <div className="details">
-              <h2>Barista</h2>
-              <p>Diall 9812345678</p>
-            </div>
-          </div>
-        </div>
+            
         </div>
       </div>
     </div>
